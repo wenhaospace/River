b35e115dbd548e535bec582349f25a73bdab99e0
Wendell <1925005015@qq.com>
2024-09-01 14:17:51 +0800

    1.Add entity&repository&service for User; 2.Add connection for MySQL database

---
+++ Backend_SpringBoot3.3.3/src/main/java/com/river/start/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.river.start.repository;
+
+import com.river.start.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
